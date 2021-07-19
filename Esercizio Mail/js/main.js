0e884b3a2f953fd57101abad72b453077f93cb3d
Luca <lucaciprani1@gmail.com>
2021-07-19 10:31:08 +0200

    adj

--- Esercizio Mail/js/main.js
+++ Esercizio Mail/js/main.js
@@ -1,10 +1,4 @@
-// Chiedi all’utente la sua email;
-// controlla che sia nella lista di chi può accedere;
-// stampa un messaggio appropriato sull’esito del controllo.
-
-// variables
-
-var emailList = ["lucaciprani@libero.it", "emaildefault@hotmail.it", "emailstandard@gmail.com", "emaillibera@virgilio.com","email5@prova.it","email6@prova.it","email7@prova.it","email8@prova.it"];
+// 1.  Chiedi all’utente la sua email;
 
 var emailUtente = prompt("Insert your email ");
 
@@ -12,22 +6,28 @@ document.getElementById("email").innerHTML = "Your email is: " + emailUtente;
 
 console.log(emailUtente);
 
+// 2. controlla che sia nella lista di chi può accedere;
+ 
+var emailList = ["lucaciprani@libero.it", "emaildefault@hotmail.it", "emailstandard@gmail.com", "emaillibera@virgilio.com","email5@prova.it","email6@prova.it","email7@prova.it","email8@prova.it"];
+
 var trovato = false;
 
 for (var i = 0; i < emailList.length; i++) {  
 
-    if(emailUtente == emailList[i] && "@") {
-        console.log("In lista può entrare");
-        trovato = true;
-        document.getElementById("in_lista").innerHTML = "Is it on the list ? " + trovato;
+    if(emailUtente == emailList[i] && "@"){
+        trovato = true ;
     }
 }
+
+// 3.  stampa un messaggio appropriato sull’esito del controllo.
 
 if (trovato == false) {
     console.log("Non in lista");
     document.getElementById("non_in_lista").innerHTML = "Is it on the list ? " + trovato;
+} else{
+    console.log("In lista può entrare");
+    document.getElementById("in_lista").innerHTML = "Is it on the list ? " + trovato;
 }
 
-
-// console 
+//  console 
 console.log(trovato);
